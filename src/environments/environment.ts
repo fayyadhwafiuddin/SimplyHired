c4455ea59f532fbc2f6cb3de7e6a604e2edc225c
fayyadh wafi <20FTT1463@student.pb.edu.bn>
2022-08-15 11:42:57 +0800

    install ionic and firebase

---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyBmYgjnlcmPBr7F-WFbBvPQAg4VUV69vcQ",
+    authDomain: "simplyhired-d8311.firebaseapp.com",
+    databaseURL: "https://simplyhired-d8311-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "simplyhired-d8311",
+    storageBucket: "simplyhired-d8311.appspot.com",
+    messagingSenderId: "869297375890",
+    appId: "1:869297375890:web:5f7eee601c91b5ac1b744b",
+    measurementId: "G-C6WVYJ6YBV"
+  }
+};
