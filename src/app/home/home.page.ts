d6d6357fc857f9edb54f84dc98fc0ba739911c8e
fayyadh wafi <20FTT1463@student.pb.edu.bn>
2022-08-20 17:50:05 +0800

    homepage connect with firebase

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, NgZone, OnInit } from '@angular/core';
+import { getApp } from 'firebase/app';
+import { getFirestore, collection, onSnapshot, doc, getDoc, getDocs, QuerySnapshot } from 'firebase/firestore';
+import { Jobs } from '../models/jobs';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit{
+  
+  public jobList: Jobs[] = [];
+  
+  constructor(private zone: NgZone) {
 
-  constructor() {}
+  }
 
+  async ngOnInit() {
+    const firebaseApp = getApp();
+    const db = getFirestore(firebaseApp);
+
+    //to get collection from firestore
+    const jobscollection = await getDocs(collection(db, 'jobposted'));
+    
+    //push data to array
+    //this also help to show to homepage
+    jobscollection.forEach((doc) => {
+      this.jobList.push({
+        company_name : doc.data().company_name,
+        img : doc.data().img,
+        job_position : doc.data().job_position,
+        location: doc.data().location,
+        more_info: doc.data().more_info,
+        requirement: doc.data().requirement,
+        salary_from: doc.data().salary_from,
+        salary_to: doc.data().salary_to,
+      });
+
+      //this is just to show if data first
+      console.log(doc.id, " => ", doc.data());
+    });
+  }
 }
+
