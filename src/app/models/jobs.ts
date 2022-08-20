6f17f20635f03f87519e3d6282f70b95ae71a5cc
fayyadh wafi <20FTT1463@student.pb.edu.bn>
2022-08-20 17:52:34 +0800

    this is adding blueprint for jobs models

---
+++ src/app/models/jobs.ts
@@ -0,0 +1,10 @@
+export interface Jobs {
+    img: string;
+    company_name: string;
+    job_position: string;
+    location: string;
+    salary_to: number;
+    salary_from: number;
+    requirement: string;
+    more_info: string;
+  }
