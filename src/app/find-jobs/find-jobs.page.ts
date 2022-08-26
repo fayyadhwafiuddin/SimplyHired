82f06698ccf6946d231882e2d0859c9e85ba93fe
fayyadh wafi <20FTT1463@student.pb.edu.bn>
2022-08-27 00:19:45 +0800

    ion-footer and button for more info model editted

--- src/app/find-jobs/find-jobs.page.ts
+++ src/app/find-jobs/find-jobs.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { getApp } from 'firebase/app';
 import { getFirestore, collection, onSnapshot, doc, getDoc, getDocs, QuerySnapshot } from 'firebase/firestore';
-import { Jobs } from '../models/jobs';
+import { Job, Jobs } from '../models/jobs';
 @Component({
   selector: 'app-find-jobs',
   templateUrl: './find-jobs.page.html',
@@ -10,6 +10,10 @@ import { Jobs } from '../models/jobs';
 export class FindJobsPage implements OnInit {
 
   public jobList: Jobs[] = [];
+  currentjob = [];
+  onjob = [];
+  jobindex : number;
+  // public job: Job = [];
 
   constructor() { }
 
@@ -32,18 +36,51 @@ export class FindJobsPage implements OnInit {
         requirement: doc.data().requirement,
         salary_from: doc.data().salary_from,
         salary_to: doc.data().salary_to,
+        post_duration: doc.data().post_duration,
       });
 
       //this is just to show if data can be retrieved
       console.log(doc.id, " => ", doc.data());
+      this.currentjob.push({
+        doc_id: doc.id,
+        company_name : doc.data().company_name,
+        img : doc.data().img,
+        job_position : doc.data().job_position,
+        location: doc.data().location,
+        more_info: doc.data().more_info,
+        requirement: doc.data().requirement,
+        salary_from: doc.data().salary_from,
+        salary_to: doc.data().salary_to,
+      });
+
     });
+    console.log(this.currentjob);
   }
 
   isModalOpen = false;
 
-  setOpen(isOpen: boolean) {
+  async setOpen(isOpen: boolean) {
+    const firebaseApp = getApp();
     this.isModalOpen = isOpen;
     console.log(this.isModalOpen);
   }
+
+ getitem(item) {
+  console.log(item);
+  console.log(this.currentjob[item]);
+
+  this.onjob = [{
+    doc_id: this.currentjob[item].doc_id,
+    company_name : this.currentjob[item].company_name,
+    img : this.currentjob[item].img,
+    job_position : this.currentjob[item].job_position,
+    location: this.currentjob[item].location,
+    more_info: this.currentjob[item].more_info,
+    requirement: this.currentjob[item].requirement,
+    salary_from: this.currentjob[item].salary_from,
+    salary_to: this.currentjob[item].salary_to,
+  }];
+  console.log(this.onjob);
+ }
 
 }
