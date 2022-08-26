d84c7e75df73666e5213ddb20eb1ba830eb16715
fayyadh wafi <20FTT1463@student.pb.edu.bn>
2022-08-27 00:11:58 +0800

    adding job model

--- src/app/models/jobs.ts
+++ src/app/models/jobs.ts
@@ -7,4 +7,16 @@ export interface Jobs {
     salary_from: number;
     requirement: string;
     more_info: string;
+    post_duration: number;
+  }
+
+  export interface Job {
+    img: string;
+    company_name: string;
+    job_position: string;
+    location: string;
+    salary_to: number;
+    salary_from: number;
+    requirement: string;
+    more_info: string;
   }
